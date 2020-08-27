0c7b4c8cacb5247061cf6bd72fe37b49aebc5e25
Stefaan <safwebdev@gmail.com>
2020-08-28 05:40:10 +0800

    Setup MongoDB

---
+++ models/Note.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const NoteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Please add a title"],
+    unique: true,
+    trim: true,
+    maxlength: [50, "Title cannot be more than 50 Characters"],
+  },
+  description: {
+    type: String,
+    required: true,
+    maxlength: [250, "Description cannot be more than 250 Characters"],
+  },
+});
+
+mongoose.exports = mongoose.models.Note || mongoose.model("Note", NoteSchema);
---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    MONGO_URI:
+      "mongodb+srv://safwbdev:lolCAT25@merntodolist.pjdg9.mongodb.net/keepClone?retryWrites=true&w=majority",
+  },
+};
+
+// "mongodb+srv://safwbdev:lolCAT25@merntodolist.pjdg9.mongodb.net/<dbname>?retryWrites=true&w=majority";
