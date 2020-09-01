11146a23a45e835f418a828dd19d5ab78589357c
Stefaan <safwebdev@gmail.com>
2020-09-01 17:01:34 +0800

    updates

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  env: {
-    MONGO_URI:
-      "mongodb+srv://safwbdev:lolCAT25@merntodolist.pjdg9.mongodb.net/keepClone?retryWrites=true&w=majority",
-  },
-};
-
-// "mongodb+srv://safwbdev:lolCAT25@merntodolist.pjdg9.mongodb.net/<dbname>?retryWrites=true&w=majority";
